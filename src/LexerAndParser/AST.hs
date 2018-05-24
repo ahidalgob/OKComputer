47458411341f3c67f772df6396948f08cfa08dec
alexanderzx360 <alexanderromero360@gmail.com>
2018-05-24 10:45:07 -0400

    Working on AST printing

--- src/LexerAndParser/AST.hs
+++ src/LexerAndParser/AST.hs
@@ -1,5 +1,6 @@
 module AST where
 import SymTable(Id, SymId)
+import Control.Monad
 
 data STARTN = STARTN [IMPORTN] [OUTSIDEN] deriving Show
 
@@ -96,3 +97,79 @@ data EXPRESSIONSTRUCTN = EXPRESSIONSTRUCTN String String deriving Show
 
 data FUNCTIONCALLN = FUNCTIONCALLN String [Id] deriving Show
 
+ident = "|  "
+
+putStrWithIdent n s = (replicateM_ n $ putStr ident) >> putStr s
+
+putStrLnWithIdent n s = (replicateM_ n $ putStr ident) >> putStrLn s
+
+printId n s = putStrLnWithIdent n $ "ID: " ++ s
+
+printExpN :: Int -> EXPRESSIONN -> IO()
+printExpN n (IDEXPRESSION s) = do
+    printId n s
+
+printExpN n (NUMBEREXPN s) = do
+    putStrLnWithIdent n $ "Literal numerico: " ++ s
+
+printExpN n (OKN) = do
+    putStrLnWithIdent n "Literal booleano: true"
+     
+printExpN n (NOTOKN) = do
+    putStrLnWithIdent n "Literal booleano: false"
+
+printExpN n (PARENTESISN exp) = do
+    putStrLnWithIdent n "Expresion entre parentesis:"
+    printExpN (n+1) exp
+     
+printExpN n (COMPARN exp s exp1) = do
+    putStrLnWithIdent n "Operacion de comparacion:"
+    putStrLnWithIdent (n+1) $ "Comparador: " ++ s
+    putStrLnWithIdent (n+1) "Lado izquierdo:"
+    printExpN (n+2) exp
+    putStrLnWithIdent (n+1) "Lado derecho:"
+    printExpN (n+2) exp1
+    
+printExpN n (NOTN exp) = do
+    putStrLnWithIdent n "Negacion booleana:"
+    printExpN (n+2) exp
+
+printExpN n (LOGICN exp s exp1) = do
+    putStrLnWithIdent n "Operacion binaria logica:"
+    putStrLnWithIdent (n+1) $ "Operador: " ++ s
+    putStrLnWithIdent (n+1) "Lado izquierdo:"
+    printExpN (n+2) exp
+    putStrLnWithIdent (n+1) "Lado derecho:"
+    printExpN (n+2) exp1
+
+printExpN n (MINUSN s exp) = do
+    putStrLnWithIdent n "Menos unario:"
+    printExpN (n+1) exp
+
+printExpN n (ARITN exp s exp1) = do
+    putStrLnWithIdent n "Operacion binaria aritmetica:"
+    putStrLnWithIdent (n+1) $ "Operador: " ++ s
+    putStrLnWithIdent (n+1) "Lado izquierdo:"
+    printExpN (n+2) exp
+    putStrLnWithIdent (n+1) "Lado derecho:"
+    printExpN (n+2) exp1
+
+--printExpN n (FUNCCALLN s exp) = do 
+ --   putStrLnWithIdent n "Llamada de funcion:"
+ --   printId (n+1) s
+ --   printExpListN (n+1) exp
+    
+
+printSTARTN :: Int -> STARTN -> IO()
+printSTARTN n (STARTN imports outsides) = do
+    putStrLnWithIdent n "Constructor de Programa:"
+    mapM_ (printImportN (n+1)) imports
+    mapM_ (printFuncListN (n+1)) outsides
+
+printImportN :: Int -> IMPORTN -> IO()
+printImportN n (IMPORTN ids) = do
+	putStrWithIdent n "Por aqui voy bien"
+
+printFuncListN :: Int -> OUTSIDEN -> IO()
+printFuncListN n (OUTFUNCTIONINIC funciones) = do 
+	putStrWithIdent n "Por aqui tambien"
